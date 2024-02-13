1e52c0534fb1bf4b7ac344ed2394ee2a5b5b7b46
Arjun P <arjunp@jmangroup.com>
2024-02-13 11:27:55 +0530

    feat: added cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const myRoute = require('./routes/allRoutes');
-
-
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 app.use(express.json()); 
 app.use(bodyParser.json());
+app.use(cors())
 app.use('/api', myRoute);  
 
 
