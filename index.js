34a65edcc1d58414a67c357399bd7c8de3149c2b
Arjun P <arjunp@jmangroup.com>
2024-02-16 17:57:11 +0530

    feature: added build to backend

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 const myRoute = require('./routes/allRoutes');
 const bodyParser = require('body-parser');
 var cors = require('cors')
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors())
-app.use('/api', myRoute);  
+app.use('/api', myRoute);
+
+
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public'));
+})
 
 
 app.listen(3000, () => {
